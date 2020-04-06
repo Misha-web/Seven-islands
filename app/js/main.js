c37b62966a61610470b9a24c25d7b8bb27c3e367
Misha <mirzoevmisha7@gmail.com>
2020-04-06 12:09:46 +0300

    site tabs is made

--- app/js/main.js
+++ app/js/main.js
@@ -1,56 +1,14 @@
 $(document).ready(function() {
-	$(".slider").not(":first").hide();
-	$(".projects__wrapper .projects__header-tab").click(function(){
-		$(".projects__wrapper .projects__header-tab").removeClass("active").eq($(this).index()).addClass("active");
-		$(".slider").hide().eq($(this).index()).fadeIn();
-		$(".slider").slick("refresh");
-	}).eq(0).addClass("active");
 
-	$(".slider").slick({
-		dots: true,
-		infinite: true,
-		speed: 1000,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive:[
-		{
-			breakpoint: 880,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			}
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		},
-		]
-	});
+	$(".js-tab-trigger").click(function(){
+		let id = $(this).attr('data-tab'),
+		content = $('.js-tab-content[data-tab="'+ id +'"]');
 
-	$(".skills__slider").slick({
-		infinite: true,
-		speed: 1000,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive:[
-		{
-			breakpoint: 880,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 2
-			}
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1
-			}
-		},
-		]
+		$(".js-tab-trigger.active").removeClass("active");
+		$(this).addClass("active");
+
+		$(".js-tab-content.active").removeClass("active");
+		content.addClass("active");
 	});
 
 });
