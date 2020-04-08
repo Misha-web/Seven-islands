934ebdbbfaea82420683c90587b57c488ddc2124
Misha <mirzoevmisha7@gmail.com>
2020-04-08 19:18:27 +0300

    site certificate and about is made

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,35 @@ $(document).ready(function() {
 		content.addClass("active");
 	});
 
+	$(".slider").slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+		{
+			breakpoint: 1000,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+    ]
+});
+
 });
 
 
