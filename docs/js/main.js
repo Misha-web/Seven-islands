a3add28ed90cb31741f726a38b61ee32e97bd840
Misha <mirzoevmisha7@gmail.com>
2020-04-11 21:22:52 +0300

    optimized map

--- docs/js/main.js
+++ docs/js/main.js
@@ -108,6 +108,19 @@ $(document).ready(function() {
 	$(".close").click(function(){
 		$('.popup').removeClass('show');
 	});
+
+	// Map
+
+	let awards = $('.awards');
+	let	awardsTop = awards.offset().top;
+	$(window).bind('scroll', function() {
+		let windowTop = $(this).scrollTop();
+		if (windowTop > awardsTop) {
+			$('.footer__map').html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1715.370201374641!2d40.521817216288!3d64.53644298377304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x44183681aa449a41%3A0x84c7599b6e00d489!2z0L_RgNC-0YHQvy4g0KfRg9C80LHQsNGA0L7QstCwLdCb0YPRh9C40L3RgdC60L7Qs9C-LCAzOSwgMjgsINCQ0YDRhdCw0L3Qs9C10LvRjNGB0LosINCQ0YDRhdCw0L3Qs9C10LvRjNGB0LrQsNGPINC-0LHQuy4sINCg0L7RgdGB0LjRjywgMTYzMTAw!5e0!3m2!1sru!2sua!4v1586511267819!5m2!1sru!2sua" allowfullscreen="" aria-hidden="false" tabindex="12"></iframe>')
+			$(window).unbind('scroll');
+		}
+	});
+
 });
 
 
